7da273c8b2465c92a1287ab28f763ca9e5b9fe1c
lletierce <letierce.lo@gmail.com>
2023-01-18 16:10:56 +0100

    ✨ F01: LOGIN > Add title and form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import LoginPage from "./components/pages/LoginPage"
 
 function App() {
+  // state (état, données)
+  // comportements
+  // affichage (render)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Salut Loris
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <LoginPage />
+  )
 }
 
-export default App;
+export default App
--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,5 +1,34 @@
+import { useState } from "react"
+
 export default function LoginPage() {
-  return (
-    <div>LoginPage</div>
-  )
+    // state (état, données)
+    const [prenom, setPrenom] = useState("")
+
+    // comportements
+    const handleSubmit = () => {
+        alert(`Bonjour ${prenom}`)
+    }
+
+    const handleChange = (event) => {
+        setPrenom(event.target.value)
+    }
+
+    // affichage (render)
+    return (
+        <div>
+            <h1>Bienvenue chez nous !</h1>
+            <br />
+            <h3>Connectez-vous</h3>
+            <form action="submit" onSubmit={handleSubmit}>
+                <input
+                    value={prenom}
+                    type="text"
+                    placeholder="Entrez votre prénom..."
+                    onChange={handleChange}
+                    required
+                />
+                <button>Accédez à votre espace</button>
+            </form>
+        </div>
+    )
 }
