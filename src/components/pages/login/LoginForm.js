2b73bfde48a8705a229e358b2280e6abb1aa757c
lletierce <letierce.lo@gmail.com>
2023-01-23 11:18:03 +0100

    ✨ F02: LOGIN > add redirection from/to OrderPage

--- src/components/pages/login/LoginForm.js
+++ src/components/pages/login/LoginForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 export default function LoginForm() {
 
@@ -31,6 +32,7 @@ export default function LoginForm() {
                     required
                 />
                 <button>Accédez à votre espace</button>
+                <Link to="/order">Vers OrderPage</Link>
             </form>
         </div>
     )
