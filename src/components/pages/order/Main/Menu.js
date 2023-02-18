51576ec18aada0b03d5c5ee9379cd11c173c8e74
lletierce <letierce.lo@gmail.com>
2023-02-18 19:14:59 +0100

    💄 F05: ORDER > Menu > update  style

--- src/components/pages/order/Main/Menu.js
+++ src/components/pages/order/Main/Menu.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import styled from "styled-components";
 import {fakeMenu2} from "../../../../fakeData/fakeMenu"
+import { theme } from "../../../../theme";
 
 export default function Menu() {
 
@@ -16,12 +17,13 @@ export default function Menu() {
 }
 
 const MenuStyled = styled.div`
-  background: purple;
+  background: ${theme.colors.background_white};
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   grid-row-gap: 60px;
   padding: 50px 50px 150px;
   justify-items: center;
+  box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
 
   .produit{
     background: red;
