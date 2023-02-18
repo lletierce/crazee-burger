c4fe50cd574520babf575208cba89a42c529fe98
lletierce <letierce.lo@gmail.com>
2023-02-18 18:16:03 +0100

    ♻️ F05: ORDER > Main > Create Menu

--- src/components/pages/order/Main/Main.js
+++ src/components/pages/order/Main/Main.js
@@ -1,11 +1,12 @@
 import styled from "styled-components";
 import { theme } from "../../../../theme";
+import Menu from "./Menu";
 
 export default function Main() {
   return (
     <MainStyled>
       <div className="basket">Basket</div>
-      <div className="menu">Menu</div>
+      <Menu />
     </MainStyled>
   )
 }
@@ -23,8 +24,5 @@ const MainStyled = styled.div`
 
         .basket{
           background: pink;
-        }
-        .menu{
-          background: purple;
         }
 `;
---
+++ src/components/pages/order/Main/Menu.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export default function Menu() {
+    return (
+        <MenuStyled>
+            Menu
+        </MenuStyled>
+    )
+}
+
+const MenuStyled = styled.div`
+  background: purple;
+`;
