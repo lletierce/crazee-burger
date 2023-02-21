aeffa740e283075ce2306e6bfeeaf4c52c0178e1
lletierce <letierce.lo@gmail.com>
2023-02-21 17:21:39 +0100

    💄 F05: ORDER > Menu > Product > Add final style

--- src/components/pages/order/Main/Menu.js
+++ src/components/pages/order/Main/Menu.js
@@ -1,18 +1,24 @@
-import { useState } from "react";
-import styled from "styled-components";
-import { fakeMenu1, fakeMenu2 } from "../../../../fakeData/fakeMenu"
-import { theme } from "../../../../theme";
-import Product from "./Product";
+import { useState } from "react"
+import styled from "styled-components"
+import { fakeMenu2 } from "../../../../fakeData/fakeMenu"
+import { theme } from "../../../../theme"
+import { formatPrice } from "../../../../utils/maths"
+import Card from "../../../reusable-ui/Card"
 
 export default function Menu() {
-
-  const [menu, setMenu] = useState(fakeMenu1)
+  const [menu, setMenu] = useState(fakeMenu2)
 
   return (
-    <MenuStyled>
-      {menu.map((produit) => {
-        // return <Product title={produit.title} imageSource={produit.imageSource} price={produit.price} />
-        return <Product {...produit} />
+    <MenuStyled className="menu">
+      {menu.map(({ id, title, imageSource, price }) => {
+        return (
+          <Card
+            key={id}
+            title={title}
+            imageSource={imageSource}
+            leftDescription={formatPrice(price)}
+          />
+        )
       })}
     </MenuStyled>
   )
@@ -26,4 +32,4 @@ const MenuStyled = styled.div`
   padding: 50px 50px 150px;
   justify-items: center;
   box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
-`;
+`
