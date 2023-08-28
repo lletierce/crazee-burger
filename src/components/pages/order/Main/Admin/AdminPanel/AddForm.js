1213676f7fc3b4237050ae07c1e1147fccbf131f
lletierce <letierce.lo@gmail.com>
2023-08-28 15:06:02 +0200

    🚧 F08: ADMIN > AddForm > Add inputs layout

--- src/components/pages/order/Main/Admin/AdminPanel/AddForm.js
+++ src/components/pages/order/Main/Admin/AdminPanel/AddForm.js
@@ -1,5 +1,44 @@
+import styled from 'styled-components';
+
 export default function AddForm() {
   return (
-    <div>AddForm</div>
+    <AddFormStyled>
+      <div className='image-preview'>Image preview</div>
+      <div className='input-fields'>
+        <input type='text' placeholder='Nom'/>
+        <input type='text' placeholder='Image URL'/>
+        <input type='text' placeholder='Price'/>
+      </div>
+      <button className='submit-button'>Submit button</button>
+    </AddFormStyled>
   )
 }
+
+const AddFormStyled = styled.form`
+  border: 2px solid black;
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+  grid-template-rows: repeat(4, 1fr);
+
+  height: 100%;
+  width: 70%;
+
+  .image-preview{
+    background: red;
+    grid-area: 1 / 1 / 4 / 2;
+  }
+
+  .input-fields{
+    background: blue;
+    grid-area: 1 / 2 / -2 / -1;
+
+    display: grid;
+  }
+
+  .submit-button{
+    background: green;
+    grid-area: 4 / -2 / -1 / -1;
+
+    width: 50%;
+  }
+`;
