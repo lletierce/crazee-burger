fa260e715e133e1d5cd3b0b845008980d34b53cd
lletierce <letierce.lo@gmail.com>
2023-08-29 19:12:45 +0200

    ✨ F08: ADMIN > AddForm > Add image preview

--- src/components/pages/order/Main/Admin/AdminPanel/AddForm.js
+++ src/components/pages/order/Main/Admin/AdminPanel/AddForm.js
@@ -33,7 +33,11 @@ export default function AddForm() {
   // affichage
   return (
     <AddFormStyled onSubmit={handleSubmit}>
-      <div className="image-preview">Aucune image</div>
+      <div className="image-preview">
+        {newProduct.imageSource ?
+          (<img src={newProduct.imageSource} alt={newProduct.title}/>) :
+          (<div>Aucune image</div>)}
+      </div>
       <div className="input-fields">
         <input
           name="title"
@@ -74,6 +78,17 @@ const AddFormStyled = styled.form`
   .image-preview {
     background: red;
     grid-area: 1 / 1 / 4 / 2;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: contain;
+      object-position: center;
+    }
   }
 
   .input-fields {
