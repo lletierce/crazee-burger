d4d5b3223008f3e34af1ab9308c7435ead0fed24
letierce.lo <letierce.lo@gmail.com>
2024-02-22 18:09:00 +0100

    🚧 F15 : FORM > Add selects (ugly)

--- src/components/pages/order/Main/MainRightSide/Admin/AdminPanel/Form.js
+++ src/components/pages/order/Main/MainRightSide/Admin/AdminPanel/Form.js
@@ -27,6 +27,14 @@ const Form = React.forwardRef(({ product, onSubmit, onChange, onFocus, onBlur, c
             ref={ref && input.name === "title" ? ref : null}
           />
         ))}
+        <select name="isAvailbe" className="is-avaible" id="3">
+          <option value={true}>En stock</option>
+          <option value={false}>En rupture</option>
+        </select>
+        <select name="isPublicised" className="is-publicised" id="4">
+          <option value={true}>Sans pub</option>
+          <option value={false}>Avec pub</option>
+        </select>
       </div>
       <div className="form-footer">{children}</div>
     </FormStyled>
@@ -46,13 +54,14 @@ const FormStyled = styled.form`
   grid-row-gap: 8px;
 
   .input-fields {
-    background: blue;
+    /* background: blue; */
     grid-area: 1 / 2 / -2 / 3;
 
     display: grid;
     grid-template-rows: repeat(3, 1fr);
     grid-template-columns: repeat(3, 1fr);
     grid-row-gap: 8px;
+    grid-column-gap: 8px;
 
     .title{
       grid-area: 1 / 1 / 2 / 4;
@@ -63,6 +72,12 @@ const FormStyled = styled.form`
     .price{
       background: red;
       grid-area: 3 / 1 / 4 / 2;
+    }
+    .is-avaible{
+      background: pink;
+    }
+    .is-publicised{
+      background : yellow;
     }
   }
 
