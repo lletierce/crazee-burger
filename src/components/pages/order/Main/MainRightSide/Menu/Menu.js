f0dd4d749938e128fadce6a9e1285b7d4bab4246
letierce.lo <letierce.lo@gmail.com>
2024-02-26 18:16:40 +0100

    🚧 F15 : AVAILABILITY > MENU > Add message "Épuisé" when product is unavailable

--- src/components/pages/order/Main/MainRightSide/Menu/Menu.js
+++ src/components/pages/order/Main/MainRightSide/Menu/Menu.js
@@ -12,6 +12,7 @@ import { isEmpty } from "../../../../../../utils/array"
 import Loader from "./Loader"
 import { CSSTransition, TransitionGroup } from "react-transition-group"
 import { menuAnimation } from "../../../../../../theme/animations"
+import { convertStringToBoolean } from "../../../../../../utils/string"
 
 export default function Menu() {
   const {
@@ -51,7 +52,7 @@ export default function Menu() {
 
   return (
     <TransitionGroup component={MenuStyled} className="menu">
-      {menu.map(({ id, title, imageSource, price }) => {
+      {menu.map(({ id, title, imageSource, price, isAvailable }) => {
         return (
           <CSSTransition classNames={"menu-animation"} key={id} timeout={300}>
             <Card
@@ -65,7 +66,7 @@ export default function Menu() {
               isSelected={checkIfProductIsClicked(id, productSelected.id)}
               onAdd={(event) => handleAddButton(event, id)}
               overlapImageSource={IMAGE_NO_STOCK}
-              isOverlapImageVisible={true}
+              isOverlapImageVisible={convertStringToBoolean(isAvailable) === false}
             />
           </CSSTransition>
         )
