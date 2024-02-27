fd5dc07155bd5edd8dbbf4b8a993114754d7f5fa
letierce.lo <letierce.lo@gmail.com>
2024-02-27 17:35:07 +0100

    ✨F15 : PUBLICITY > BASKET > Card > Add new badge when product isPublicised

--- src/components/pages/order/Main/Basket/BasketBody/BasketProducts.js
+++ src/components/pages/order/Main/Basket/BasketBody/BasketProducts.js
@@ -1,7 +1,10 @@
 import React from "react";
 import { useContext } from "react";
 import styled from "styled-components/macro";
-import { BASKET_MESSAGE, IMAGE_COMING_SOON } from "../../../../../../enums/product";
+import {
+  BASKET_MESSAGE,
+  IMAGE_COMING_SOON,
+} from "../../../../../../enums/product";
 import BasketCard from "./BasketCard";
 import OrderContext from "../../../../../../context/OrderContext";
 import { findObjectById } from "../../../../../../utils/array";
@@ -10,6 +13,7 @@ import { TransitionGroup, CSSTransition } from "react-transition-group";
 import { basketAnimation } from "../../../../../../theme/animations";
 import { formatPrice } from "../../../../../../utils/maths";
 import { convertStringToBoolean } from "../../../../../../utils/string";
+import Sticker from "../../../../../reusable-ui/Sticker";
 
 export default function BasketProducts() {
   const {
@@ -42,6 +46,9 @@ export default function BasketProducts() {
             timeout={300}
           >
             <div className="card-container">
+              {convertStringToBoolean(menuProduct.isPublicised) && (
+                <Sticker className="badge-new" />
+              )}
               <BasketCard
                 {...menuProduct}
                 imageSource={
@@ -88,12 +95,22 @@ const BasketProductsStyled = styled.div`
     margin: 10px 16px;
     height: 86px;
     box-sizing: border-box;
+    position: relative;
     :first-child {
       margin-top: 20px;
       /* border: 1px solid red; */
     }
     :last-child {
       margin-bottom: 20px;
+    }
+
+    .badge-new {
+      position: absolute;
+      z-index: 1;
+      bottom: 10%;
+      left: 21%;
+      transform: translateY(-21%);
+      transform: translateX(-5%);
     }
   }
 
