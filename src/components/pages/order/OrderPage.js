d269db5ad817f2a2f7be069c6973a0e88c9de713
letierce.lo <letierce.lo@gmail.com>
2024-04-30 19:14:39 +0200

    🚧 UI02 : MENU > Conditional rendering for screen size

--- src/components/pages/order/OrderPage.js
+++ src/components/pages/order/OrderPage.js
@@ -1,39 +1,47 @@
-import { useEffect, useRef, useState } from "react"
-import styled from "styled-components"
-import { theme } from "../../../theme"
-import Main from "./Main/Main"
-import Navbar from "./Navbar/Navbar"
-import OrderContext from "../../../context/OrderContext"
-import { EMPTY_PRODUCT } from "../../../enums/product"
-import { useMenu } from "../../../hooks/useMenu"
-import { useBasket } from "../../../hooks/useBasket"
-import { findObjectById } from "../../../utils/array"
-import { useParams } from "react-router-dom"
-import { initialiseUserSession } from "./helpers/initialiseUserSession"
+import { useEffect, useRef, useState } from "react";
+import styled from "styled-components";
+import { theme } from "../../../theme";
+import Main from "./Main/Main";
+import Navbar from "./Navbar/Navbar";
+import OrderContext from "../../../context/OrderContext";
+import { EMPTY_PRODUCT } from "../../../enums/product";
+import { useMenu } from "../../../hooks/useMenu";
+import { useBasket } from "../../../hooks/useBasket";
+import { findObjectById } from "../../../utils/array";
+import { useParams } from "react-router-dom";
+import { initialiseUserSession } from "./helpers/initialiseUserSession";
+
+import OrderPageMobile from "./OrderPageMobile";
+import { useWindowDimensions } from "react-native-web";
 
 export default function OrderPage() {
   // state
-  const [isModeAdmin, setIsModeAdmin] = useState(false)
-  const [isCollapsed, setIsCollapsed] = useState(false)
-  const [currentTabSelected, setCurrentTabSelected] = useState("add")
-  const [newProduct, setNewProduct] = useState(EMPTY_PRODUCT)
-  const [productSelected, setProductSelected] = useState(EMPTY_PRODUCT)
-  const titleEditRef = useRef()
-  const { menu, setMenu, handleAdd, handleDelete, handleEdit, resetMenu } = useMenu()
-  const { basket, setBasket, handleAddToBasket, handleDeleteBasketProduct } = useBasket()
-  const { username } = useParams()
+  const [isModeAdmin, setIsModeAdmin] = useState(false);
+  const [isCollapsed, setIsCollapsed] = useState(false);
+  const [currentTabSelected, setCurrentTabSelected] = useState("add");
+  const [newProduct, setNewProduct] = useState(EMPTY_PRODUCT);
+  const [productSelected, setProductSelected] = useState(EMPTY_PRODUCT);
+  const titleEditRef = useRef();
+  const { menu, setMenu, handleAdd, handleDelete, handleEdit, resetMenu } =
+    useMenu();
+  const { basket, setBasket, handleAddToBasket, handleDeleteBasketProduct } =
+    useBasket();
+  const { username } = useParams();
+  const [isDesktop, setIsDesktop] = useState(window.innerWidth);
+
+  const { height, width } = useWindowDimensions();
 
   const handleProductSelected = async (idProductClicked) => {
-    const productClickedOn = findObjectById(idProductClicked, menu)
-    await setIsCollapsed(false)
-    await setCurrentTabSelected("edit")
-    await setProductSelected(productClickedOn)
-    titleEditRef.current.focus()
-  }
+    const productClickedOn = findObjectById(idProductClicked, menu);
+    await setIsCollapsed(false);
+    await setCurrentTabSelected("edit");
+    await setProductSelected(productClickedOn);
+    titleEditRef.current.focus();
+  };
 
   useEffect(() => {
-    initialiseUserSession(username, setMenu, setBasket)
-  }, [])
+    initialiseUserSession(username, setMenu, setBasket);
+  }, []);
 
   const orderContextValue = {
     username,
@@ -57,19 +65,23 @@ export default function OrderPage() {
     handleAddToBasket,
     handleDeleteBasketProduct,
     handleProductSelected,
-  }
+  };
 
   //affichage (render)
   return (
     <OrderContext.Provider value={orderContextValue}>
       <OrderPageStyled>
-        <div className="container">
-          <Navbar />
-          <Main />
-        </div>
+        {width > 520 ? (
+          <div className="container">
+            <Navbar />
+            <Main />
+          </div>
+        ) : (
+          <OrderPageMobile />
+        )}
       </OrderPageStyled>
     </OrderContext.Provider>
-  )
+  );
 }
 
 const OrderPageStyled = styled.div`
@@ -87,4 +99,4 @@ const OrderPageStyled = styled.div`
     flex-direction: column;
     border-radius: ${theme.borderRadius.extraRound};
   }
-`
+`;
