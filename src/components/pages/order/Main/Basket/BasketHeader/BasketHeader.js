89168644dde608982b566758a2ac74bf838bc771
letierce.lo <letierce.lo@gmail.com>
2024-09-11 18:18:16 +0200

    📱 UI02 : Manage basket displaying for mobile screen

--- src/components/pages/order/Main/Basket/BasketHeader/BasketHeader.js
+++ src/components/pages/order/Main/Basket/BasketHeader/BasketHeader.js
@@ -1,25 +1,34 @@
-import { useContext } from "react"
-import styled from "styled-components"
-import OrderContext from "../../../../../../context/OrderContext"
-import { theme } from "../../../../../../theme"
-import { formatPrice } from "../../../../../../utils/maths"
-import Header from "../../../../../reusable-ui/Header"
-import { calculateSumToPay } from "./helper"
-import CasinoEffect from "../../../../../reusable-ui/CasinoEffect"
+import { useContext } from "react";
+import styled, { css } from "styled-components";
+import OrderContext from "../../../../../../context/OrderContext";
+import { theme } from "../../../../../../theme";
+import { formatPrice } from "../../../../../../utils/maths";
+import Header from "../../../../../reusable-ui/Header";
+import { calculateSumToPay } from "./helper";
+import CasinoEffect from "../../../../../reusable-ui/CasinoEffect";
+import { useWindowDimensions } from "react-native-web";
 
 export default function BasketHeader() {
-  const { basket, menu } = useContext(OrderContext)
 
-  const sumToPay = calculateSumToPay(basket, menu)
+  const { width } = useWindowDimensions(); // TODO : remove duplicate
+
+  const { basket, menu, isBasketVisible, setIsBasketVisible } =
+    useContext(OrderContext);
+
+  const sumToPay = calculateSumToPay(basket, menu);
+  const handleBasketDisplaying = () => {
+    setIsBasketVisible(!isBasketVisible);
+  };
 
   return (
     <Header>
-      <BasketHeaderStyled>
+      <BasketHeaderStyled width={width}>
         <span className="total">Total</span>
         <CasinoEffect count={formatPrice(sumToPay)} />
+        {width <= theme.breakpoints.screen.md && <div  className="reduce-basket-icon" onClick={handleBasketDisplaying}>{"<"}</div>}
       </BasketHeaderStyled>
     </Header>
-  )
+  );
 }
 
 const BasketHeaderStyled = styled.div`
@@ -29,7 +38,22 @@ const BasketHeaderStyled = styled.div`
   align-items: center;
   color: ${theme.colors.primary};
   font-family: "Amatic SC", cursive;
+  
+  ${({ width}) => width <= theme.breakpoints.screen.md ? fontMobileStyled : fontStyled}
+
+  .reduce-basket-icon{
+    cursor: pointer;
+  }
+`;
+
+const fontStyled = css`
   font-size: ${theme.fonts.size.P4};
   font-weight: ${theme.fonts.weights.bold};
   letter-spacing: 2px;
-`
+`;
+
+const fontMobileStyled = css`
+  font-size: ${theme.fonts.size.P3};
+  font-weight: ${theme.fonts.weights.bold};
+  letter-spacing: 1px;
+`;
--- src/components/pages/order/orderMobile/ContainerOrderPageMobile.js
+++ src/components/pages/order/orderMobile/ContainerOrderPageMobile.js
@@ -1,15 +1,54 @@
-import React from 'react'
-import styled from 'styled-components';
-import NavbarMobile from './NavbarMobile';
+import React, { useContext } from "react";
+import styled from "styled-components";
+import NavbarMobile from "./NavbarMobile";
+import OrderContext from "../../../../context/OrderContext";
+import { calculateSumToPay } from "../Main/Basket/BasketHeader/helper";
+import { formatPrice } from "../../../../utils/maths";
+import Loader from "../Main/MainRightSide/Menu/Loader";
+import Basket from "../Main/Basket/Basket";
+import Admin from "../Main/MainRightSide/Admin/Admin";
+import { FaShoppingBasket } from "react-icons/fa";
+import { theme } from "../../../../theme";
+
 
 export default function ContainerOrderPageMobile() {
+  const { basket, menu, isModeAdmin, isBasketVisible, setIsBasketVisible } =
+    useContext(OrderContext);
+
+  const sumToPay = calculateSumToPay(basket, menu);
+
+  // state
+
+  // comportement
+  const handleBasketDisplaying = () => {
+    setIsBasketVisible(!isBasketVisible);
+  };
+
+  // affichage
+  if (menu === undefined) return <Loader />; // TODO : remove duplicate
+
   return (
     <ContainerOrderPageMobileStyled>
       <NavbarMobile />
-      <div className='toolbar'>toolbar</div>
+      
+      {/* TODO : Refacto and Add animations */}
+      {!isBasketVisible && (
+        <div className="toolbar">
+        <div  className="icon-basket" onClick={handleBasketDisplaying}>
+          <FaShoppingBasket />
+        </div>
+        <div className="price">{formatPrice(sumToPay)}</div>
+      </div>
+      )}
       <div>menu</div>
+      {isBasketVisible && (
+        <div className="lateral-panel">
+          <Basket />
+        </div>
+      )}
+      {isModeAdmin && <Admin />}
     </ContainerOrderPageMobileStyled>
-  )
+  );
 }
 
 const ContainerOrderPageMobileStyled = styled.div`
@@ -21,9 +60,45 @@ const ContainerOrderPageMobileStyled = styled.div`
   flex-direction: column;
   /* align-items: center; */
 
-
-  .toolbar{
-    background-color: lightgreen;
+  .toolbar {
+    /* background-color: lightgreen; */
+    background: ${theme.colors.background_dark};
+    /* color: ${theme.colors.dark}; */
     height: 5vh;
+
+    display: flex;
+    align-items: center;
+    padding-left: 20px;
+    column-gap: 10px;
+
+    box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
+    /* font-family: "Amatic SC", cursive; */
+
+    .icon-basket{
+      color: ${theme.colors.white};
+      display: flex;
+      align-items: center;
+      cursor: pointer;
+
+
+      transform: scale(1.5);
+
+    }
+
+    .price{
+      color: ${theme.colors.primary};
+      /* font-weight: ${theme.fonts.weights.semiBold}; */
+    }
+  }
+
+  .lateral-panel {
+    position: absolute;
+    z-index: 1;
+    top: 0;
+    left: 0;
+    height: 90vh;
+    width: 50vw;
+    background-color: yellow;
+    margin-top: 10vh;
   }
 `;
